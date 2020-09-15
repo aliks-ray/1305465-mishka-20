93f1317f4a6acb21bbb70d4a5f0acc38d92d79b3
Alexandra K <64307537+aliks-ray@users.noreply.github.com>
2020-09-15 20:33:58 +0300

    Правки после проверки на допуск

--- source/js/script.js
+++ source/js/script.js
@@ -1,18 +1,20 @@
 const modalBasket = document.querySelector(".modal");
-const toBasketButton = document.querySelector(".product-of-the-week__button");
-const basketButton = document.querySelectorAll(".product-card__button");
+const basketButton = document.querySelectorAll(".js-modal-button");
 
-toBasketButton.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  modalBasket.classList.add("modal__active");
-  modalForm.classList.add("modal-wrapper--active");
-});
+basketButton.forEach(function (btn) {
+  btn.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    modalBasket.classList.add("modal__active");
+    modalForm.classList.add("modal-wrapper--active");
+  })
+})
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     if (modalBasket.classList.contains("modal__active")) {
       evt.preventDefault();
       modalBasket.classList.remove("modal__active");
+      modalForm.classList.remove("modal-wrapper--active")
     }
   }
 });
